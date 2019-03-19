405cc6899c3d7340f4566f4cf8e123f6523b14ee
i342680 <dimo.georgiev@sap.com>
2019-03-19 10:43:54 +0200

    Refactor TagContainer

--- courseworkui/src/components/TagContainer/index.jsx
+++ courseworkui/src/components/TagContainer/index.jsx
@@ -6,10 +6,11 @@ import styles from './styles.css';
 class TagContainer extends Component {
 
     render() {
-        console.log(this.props.tagArray)
         return (
             <>
-                {this.props.tagArray.map(singleTag => <span key={singleTag} className="tag">{singleTag}<span className="close"></span></span>)}
+                {this.props.tagArray.map(singleTag =>
+                    <span key={singleTag} className="tag">{singleTag}<span className="close"></span></span>
+                )}
             </>
         );
     };
