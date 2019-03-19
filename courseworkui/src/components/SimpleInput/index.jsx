76be717612c2c74ef2cc82c1b7d054d600777a82
i342680 <dimo.georgiev@sap.com>
2019-03-19 10:11:11 +0200

    Add SimpleInput

---
+++ courseworkui/src/components/SimpleInput/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//eslint-disable-next-line
+import styles from './styles.css';
+
+
+class SimpleInput extends Component {
+
+    updateParent = (evt) => {
+        this.props.onSimpleInputChange(evt.target.value);
+    }
+
+    render() {
+        return (
+            <input className="main-input"
+                onChange={evt => this.updateParent(evt)}
+                value={this.props.inputValue} />
+        );
+    };
+}
+export default SimpleInput;
